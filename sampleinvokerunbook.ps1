37adf69582c557f05be8cadc5a8e872f5e37f667
msdirtbag <105983492+msdirtbag@users.noreply.github.com>
2024-02-21 00:17:51 -0600

    Create sampleinvokerunbook.ps1

---
+++ sampleinvokerunbook.ps1
@@ -0,0 +1,34 @@
+﻿# Set-ExecutionPolicy
+Set-ExecutionPolicy Unrestricted -Force
+
+#Set Directory
+$PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="C:\Users\Public\Downloads\tmp\atomics"}
+
+#Download Payload
+$webClient = New-Object System.Net.WebClient
+
+# Download and execute install-atomicredteam.ps1
+$scriptBytes = $webClient.DownloadData('https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1')
+$scriptString = [System.Text.Encoding]::UTF8.GetString($scriptBytes)
+Invoke-Expression -Command $scriptString
+Install-AtomicRedTeam -InstallPath "C:\Users\Public\Downloads\tmp" -Force
+
+# Download and execute install-atomicsfolder.ps1
+$scriptBytes = $webClient.DownloadData('https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicsfolder.ps1')
+$scriptString = [System.Text.Encoding]::UTF8.GetString($scriptBytes)
+Invoke-Expression -Command $scriptString
+Install-AtomicsFolder -InstallPath "C:\Users\Public\Downloads\tmp" -Force
+
+#Load framework
+Install-Module -Name invoke-atomicredteam,powershell-yaml -Force
+Import-Module -Name invoke-atomicredteam,powershell-yaml -Force
+
+#Atomics
+Invoke-AtomicTest T1078.003-1 -Verbose -Force
+Invoke-AtomicTest T1573 -Verbose -Force
+Invoke-AtomicTest T1518.001-1 -Verbose -Force
+Invoke-AtomicTest T1218.002 -Verbose -Force
+Invoke-AtomicTest T1113-2 -Verbose -Force
+
+#Clean up
+Remove-Item -LiteralPath "C:\Users\Public\Downloads\tmp" -Force -Recurse
